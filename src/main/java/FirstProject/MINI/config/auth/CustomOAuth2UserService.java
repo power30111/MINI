53932c505624d40682e45d7ac3947e3e11b47e31
power30111_HOME <power30111@naver.com>
2023-09-06 11:19:30 +0900

    update

--- src/main/java/FirstProject/MINI/config/auth/CustomOAuth2UserService.java
+++ src/main/java/FirstProject/MINI/config/auth/CustomOAuth2UserService.java
@@ -1,13 +1,36 @@
 package FirstProject.MINI.config.auth;
 
 
+import FirstProject.MINI.domain.User.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class CustomOAuth2UserService extends WebSecurityConfiguration {
 
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/user/login")
+                        .defaultSuccessUrl("/"))
+        ;
+        return http.build();
+    }
 
 }
